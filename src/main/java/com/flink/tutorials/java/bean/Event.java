99c591da2dcc45c09695bc78810edcb6216b4498
lixingliang <17623796065@163.com>
2025-01-24 17:38:56 +0800

    feat: 增加flink cep例子

---
+++ src/main/java/com/flink/tutorials/java/bean/Event.java
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.flink.tutorials.java.bean;
+
+import java.util.Objects;
+
+public class Event {
+
+    private int id;
+    private String name;
+    private String type;      // 事件类型
+    private long timestamp;   // 时间戳
+
+    public Event(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Event event = (Event) o;
+        return id == event.id && timestamp == event.timestamp && Objects.equals(name, event.name) && Objects.equals(type, event.type);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + Objects.hashCode(name);
+        result = 31 * result + Objects.hashCode(type);
+        result = 31 * result + Long.hashCode(timestamp);
+        return result;
+    }
+}
+
---
+++ src/main/java/com/flink/tutorials/java/cep/README.md
@@ -0,0 +1,5 @@
+# FlinkCEP-examples
+
+Using Apache [Flink CEP](https://github.com/apache/flink) to design simple use cases.
+Reference: https://github.com/eleniKougiou/Flink-cep-examples.git
+Each CEPCase detects an event pattern based on different contiguity conditions and after match skip strategies.
